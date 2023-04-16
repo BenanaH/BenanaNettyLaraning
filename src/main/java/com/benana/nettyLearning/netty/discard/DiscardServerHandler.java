0bba04a3a22729b54c900712a790c2af45857850
BenanaH <849681967@qq.com>
2023-04-16 17:55:15 +0800

    Discard

---
+++ src/main/java/com/benana/nettyLearning/netty/discard/DiscardServerHandler.java
@@ -0,0 +1,25 @@
+package com.benana.nettyLearning.netty.discard;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+/**
+ * @author Benana
+ * @date 2023/4/4 09:24
+ */
+public class DiscardServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) { // (2)
+        // Discard the received data silently.
+        ((ByteBuf) msg).release(); // (3)
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) { // (4)
+        // Close the connection when an exception is raised.
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
